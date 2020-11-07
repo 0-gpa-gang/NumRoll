80a507cad112b58283c01f9372f8327c465e3504
Yi Meng Wang <yimengwang@Yimengs-MacBook-Pro.local>
2020-11-07 13:06:01 -0700

    made classify.y

---
+++ classifier.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+class Classify:
+    def __init__(self):
+        # self.model = some model
+
+
+
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    test()
